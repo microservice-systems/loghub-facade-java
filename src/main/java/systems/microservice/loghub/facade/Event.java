ccd16cacfbdeede637fb4f780760956f2a1e2edd
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-12-13 02:01:22 +0300

    added Event class

--- src/main/java/systems/microservice/loghub/facade/Event.java
+++ src/main/java/systems/microservice/loghub/facade/Event.java
@@ -1,8 +1,48 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.facade;
+
+import java.io.Serializable;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class Event {
+public class Event implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public final Level level;
+    public final String logger;
+    public final Type type;
+    public final Throwable exception;
+    public final Tag tag;
+    public final Tag[] tags;
+    public final Type type1;
+    public final Type type2;
+
+    public Event(Level level, String logger, Type type, Throwable exception, Tag tag, Tag[] tags, Type type1, Type type2) {
+        this.level = level;
+        this.logger = logger;
+        this.type = type;
+        this.exception = exception;
+        this.tag = tag;
+        this.tags = tags;
+        this.type1 = type1;
+        this.type2 = type2;
+    }
 }
